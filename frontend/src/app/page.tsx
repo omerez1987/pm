4864a325bbaae0e6602e2fa19b726e89ccb20be5
Ariel Omerez <omerez1987@yahoo.com>
2026-06-28 01:07:45 -0500

    2nd commit after code review

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,4 +1,11 @@
-import { KanbanBoard } from "@/components/KanbanBoard";
+"use client";
+
+import dynamic from "next/dynamic";
+
+const KanbanBoard = dynamic(
+  () => import("@/components/KanbanBoard").then((m) => ({ default: m.KanbanBoard })),
+  { ssr: false }
+);
 
 export default function Home() {
   return <KanbanBoard />;
